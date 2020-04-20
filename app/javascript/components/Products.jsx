8429cf21fc952df93424afb20faf620d7f9f728d
Krzysztof Wiedeński <krzysiek@pop-os.localdomain>
2020-04-20 23:07:19 +0200

    Add Data Fetch and Table View

--- app/javascript/components/Products.jsx
+++ app/javascript/components/Products.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import Table from "react-bootstrap/Table";
 import axios from "axios";
 
 const Products = (props) => {
@@ -6,17 +7,36 @@ const Products = (props) => {
 
   useEffect(() => {
     axios
-      .get("")
+      .get("/products")
       .then(function (response) {
-        // handle success
+        setProducts(response.data);
         console.log(response);
       })
       .catch(function (error) {
-        // handle error
+        setProducts([]);
         console.log(error);
       });
   }, []);
-  return <h1>Products</h1>;
+  return (
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>
+        {products.map((product) => (
+          <tr>
+            <td>{product.id}</td>
+            <td>{product.name}</td>
+            <td>{product.price}</td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  );
 };
 
 export default Products;
