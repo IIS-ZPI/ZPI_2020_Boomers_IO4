1695cccba7e8e744677a5cfb84f8c47e6315e167
Krzysztof Wiedeński <krzysiek@pop-os.localdomain>
2020-04-20 17:01:11 +0200

    Fix Scss Styles Configuration

--- app/javascript/packs/App.jsx
+++ app/javascript/packs/App.jsx
@@ -2,22 +2,37 @@ import React from "react";
 import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
 
-import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/style.scss";
 
-const Hello = (props) => <h1>Hello d{props.name}!</h1>;
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 
-Hello.defaultProps = {
-  name: "David",
-};
-
-Hello.propTypes = {
-  name: PropTypes.string,
+const App = (props) => {
+  return (
+    <>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">More deets</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              Dank memes
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </>
+  );
 };
 
 document.addEventListener("DOMContentLoaded", () => {
   ReactDOM.render(
-    <Hello name="React" />,
+    <App />,
     document.body.appendChild(document.createElement("div"))
   );
 });
